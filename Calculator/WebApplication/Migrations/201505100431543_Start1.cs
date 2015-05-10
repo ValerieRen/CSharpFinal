a14c5919a952d70f351d391d9a5a898b99fb42e2
ValerieRen <renz1@hawkmail.newpaltz.edu>
2015-05-10 02:30:08 -0400

    commit

---
+++ Calculator/WebApplication/Migrations/201505100431543_Start1.cs
@@ -0,0 +1,16 @@
+namespace Calculator.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Start1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
